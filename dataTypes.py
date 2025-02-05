741d0645fbb7d44d85818e17e88d9cdbdbac6219
Jonoth bosque-garcia <jonoth.bosque-garcia@C56.local>
2025-02-05 10:17:20 -0500

    Create dataTypes.py

---
+++ dataTypes.py
@@ -0,0 +1,19 @@
+""" x =3 
+y = float(3)
+print (x,y) """
+
+""" values = [1,2.23,5,7,2,30,15]
+print(values)
+for i in values:
+    print(i)
+
+x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+x = input("What is your sentence?:")
+words = len(x.split())
+print ("Word count:" + str (words))
+
