0ae503f204b5a204379955f0998131c5aace3d69
Jonoth bosque-garcia <jonoth.bosque-garcia@C56.local>
2025-02-06 10:17:15 -0500

    Create madlibs.py

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+#Yesterday was a  _ day. School was very _. My friend _ on the other hand had a very _ day. I fnally got home and _. 
+ystr = input("Adjective") 
+skl = input("Adjective")
+frnd = input('Name')
+oth = input ('Adjective')
+fnly = input ('Verb')
+print ("Yesterday was a"+ ystr + "day" )
+print ("School was very" + skl)
+print ("My friend" + frnd "on the other hand had a very" + oth "day")
+
+
+
+
+
+""" words = len(ystr.split( ))
+words2 = len(skl.split( ))
+
+print ("Word count:" + str (words + words2)) """
