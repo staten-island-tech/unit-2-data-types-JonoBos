db473284e89ece1a3308ed512a3957e958830819
JonoBos <jonothanb@nycstudents.net>
2025-02-07 20:23:43 -0500

    Update ifELSEEEEE.py

--- ifELSEEEEE.py
+++ ifELSEEEEE.py
@@ -5,7 +5,22 @@ if (x % 2)== 0:
 else:
     print("Your number is odd") """
 
-x = int(input("How much is your bill"))
-y = int(input("Would you like to tip 0%, 15%, 20%, or 25%"))
+""" x = int(input("How much is your bill?"))
+y = int(input("Would you like to tip 0%, 15%, 20%, or 25%?"))
 if y == int(0):
-    print ("Your bill comes out to" + int(x))
+    print ("Your bill comes out to:", int(x))
+if y== int(15):
+    print("Your bill comes out to:", round(float(x * 1.15)))
+if y== int (20):
+    print ("Your bill comes out to:", round(float(x * 1.2)))
+if y == int(25):
+    print ("Your bill comes out to:",round(float(x*1.25))) """
+    
+a= int(input ("What is your number?"))
+if a <= 0:
+    print ("Your number must be greater than or equal to 1")
+if a>= 1:
+    print ("The factors of", a, "are", find_factors(a))
+    
+    
+    """ find_factors """
