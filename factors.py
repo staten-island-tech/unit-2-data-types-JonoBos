54d0314435359abb80da6434df9d8b8d0b2c19b4
Jonoth bosque-garcia <jonoth.bosque-garcia@C56.local>
2025-02-11 09:40:43 -0500

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,4 +1,11 @@
 """ def factors (x) """
-x = int(input ("What is your number?"))
-for i in range (x):
-    print ((x))
+x= int(input ("What is your number?"))
+y = int(input("What is your second number?"))
+for d in range (1,d+1):
+    if y%d ==0:
+        insert (d)
+for i in range (1, x+1):
+    if x%i ==0:
+        insert (i)
+        if d == i:
+            print (i)
